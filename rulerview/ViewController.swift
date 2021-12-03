f66808848f87fda2dc1ce5bf44c3ad390bc28df4
devdanikim <devdanikim@gmail.com>
2021-12-03 14:15:26 +0900

    Initial Commit

---
+++ rulerview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rulerview
+//
+//  Created by Dohyun Kim on 2021/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
